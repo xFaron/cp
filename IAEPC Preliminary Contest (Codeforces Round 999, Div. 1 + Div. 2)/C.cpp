a3506346c7cb3a053a807e9655ba9c1517753e8b
Harikrishna S <harikrishnas.x@gmail.com>
2025-01-26 13:11:55 +0530

    2061C - Kevin and Puzzle (My code)

--- IAEPC Preliminary Contest (Codeforces Round 999, Div. 1 + Div. 2)/C.cpp
+++ IAEPC Preliminary Contest (Codeforces Round 999, Div. 1 + Div. 2)/C.cpp
@@ -1,7 +1,71 @@
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 
 using namespace std;
+
+int isLiar[static_cast<int>(2e5 + 1)];
+int sum_[static_cast<int>(2e5 + 1)] = {0};
+int data_[static_cast<int>(2e5 + 1)];
+
+void solve() {
+    int n;
+    cin >> n;
+
+    int count = 0;
+    for (int i = 1; i <= n; i++) {
+        cin >> data_[i];
+        isLiar[i] = 0;
+    }
+    data_[n+1] = -1;
+
+    int currIndex = 1;
+    bool backing = false;
+    while (currIndex) {
+        // cout << "CurrIndex : " << currIndex << endl;
+        // for (int i = 0; i <= n + 1; i++) {
+        //     cout << i << " : " << isLiar[i] << " | " << sum_[i] << " | " << data_[i] << endl;
+        // }
+        // cout << endl;
+
+
+        if (data_[currIndex] == -1) {
+            count++;
+            backing = true;
+            currIndex--;
+        }
+
+        if (backing) {
+            if (isLiar[currIndex] || !isLiar[currIndex] && isLiar[currIndex - 1]) {
+                isLiar[currIndex] = 0;
+                currIndex--;
+            } else {
+                isLiar[currIndex] = !isLiar[currIndex];
+                sum_[currIndex] = sum_[currIndex - 1] + isLiar[currIndex];
+                currIndex++;
+                backing = false;
+            }
+            continue;
+        } else {
+            if (sum_[currIndex - 1] == data_[currIndex]) {
+                sum_[currIndex] = sum_[currIndex - 1] + isLiar[currIndex];
+                currIndex++;
+                continue;
+            } else {
+                if (isLiar[currIndex - 1]) {
+                    currIndex--;
+                    backing = true;
+                } else {
+                    isLiar[currIndex] = 1;
+                    sum_[currIndex] = sum_[currIndex - 1] + isLiar[currIndex];
+                    currIndex++;
+                }
+                continue;
+            }
+        }
+    }
+
+    cout << count << endl;
+
+}
 
 int main() {
     ios::sync_with_stdio(0);
@@ -10,22 +74,7 @@ int main() {
     int t;
     cin >> t;
 
-    while (t--) {
-        int n;
-        cin >> n;
-
-        vector<int> liar_count;
-        vector<bool> liar;
-        int count;
-
-        int currIndex = 0;
-        while (currIndex > 0) {
-            if (liar_count[currIndex - 1])
-
-
-            currIndex--;
-        }
-    }
+    while (t--) solve();
 
     return 0;
 }
